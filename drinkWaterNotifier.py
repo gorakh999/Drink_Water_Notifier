308444b99ee4c1b769b8a869ad108dfd88bf5286
raj4755 <72159048+raj4755@users.noreply.github.com>
2020-10-01 10:48:23 +0530

    Update drinkWaterNotifier.py

--- drinkWaterNotifier.py
+++ drinkWaterNotifier.py
@@ -3,7 +3,7 @@ from plyer import notification
 
 if __name__ == "__main__":
     notification.notify(
-        title = 'Please Drink Water Now'
+        title = 'Its time to drink Water'
         message = 'Human Body Need 3.7 litres of Water Everyday So please Drink Water to complete that level'
         app_icon = 'icon.ico'
         timeout = 10
